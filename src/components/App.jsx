62c36219a3156bff129c05f5032f4d23ee9ec806
Szymon1021 <105548800+Szymon1021@users.noreply.github.com>
2023-06-01 16:13:54 +0200

    rewiev

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,47 +1,26 @@
+import { useState } from 'react';
 import { Feedback } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 import { Section } from './Section/Section';
 import { Notification } from './Notifivation';
-import React, { Component } from 'react';
 
 const options = ['good', 'bad', 'neutral'];
-export class App extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    };
-  }
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [neutral, setNeutral] = useState(0);
 
-  sum = () => {
-    return this.state.good + this.state.neutral + this.state.bad;
-  };
-
-  handleIncrement = type => {
+  const handleIncrement = type => {
     switch (type) {
       case 'good':
-        this.setState(prevState => {
-          return {
-            good: prevState.good + 1,
-          };
-        });
+        setGood(good + 1);
         break;
       case 'neutral':
-        this.setState(prevState => {
-          return {
-            neutral: prevState.neutral + 1,
-          };
-        });
+        setBad(bad + 1);
         break;
       case 'bad':
-        this.setState(prevState => {
-          return {
-            bad: prevState.bad + 1,
-          };
-        });
+        setNeutral(neutral + 1);
         break;
       default:
     }
@@ -49,40 +28,35 @@ export class App extends Component {
     this.countPositiveFeedbackPercentage();
   };
 
-  countTotalFeedback = () => {
-    const total = this.state.good + this.state.neutral + this.state.bad;
+  const countTotalFeedback = () => {
+    const total = good + bad + neutral;
     return total;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const percentage = Math.round(
-      (this.state.good * 100) /
-        (this.state.good + this.state.neutral + this.state.bad)
-    );
+  const countPositiveFeedbackPercentage = () => {
+    const percentage = Math.round((good * 100) / (good + neutral + bad));
     return isNaN(percentage) ? 0 : percentage;
   };
 
-  render() {
-    return (
-      <div>
-        <Section title="please leave feedback">
-          <Feedback options={options} onLeaveFeedback={this.handleIncrement} />
-        </Section>
+  return (
+    <div>
+      <Section title="please leave feedback">
+        <Feedback options={options} onLeaveFeedback={handleIncrement} />
+      </Section>
 
-        <Section title="Statistics:">
-          {this.countTotalFeedback() === 0 ? (
-            <Notification total={this.state} message="no feedback given" />
-          ) : (
-            <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage() + '%'}
-            />
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
+      <Section title="Statistics:">
+        {countTotalFeedback() === 0 ? (
+          <Notification message="no feedback given" />
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage() + '%'}
+          />
+        )}
+      </Section>
+    </div>
+  );
+};
