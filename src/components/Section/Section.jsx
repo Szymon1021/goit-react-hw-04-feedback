2d6ee6dde55aadddf366dd9209226849b922d76a
Szymon1021 <105548800+Szymon1021@users.noreply.github.com>
2023-06-01 11:25:02 +0200

    1

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types';
+export const Section = ({ title, children }) => (
+  <>
+    <h1>{title}</h1>
+    {children}
+  </>
+);
+
+Section.propTypes = {
+  title: PropTypes.string,
+};
